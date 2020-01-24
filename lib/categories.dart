2f59f62770a143cb63314feeaf42a09ae7d34422
dickyrey <dicky.rey97@gmail.com>
2020-01-24 11:11:16 +0700

    gaming shop part i

---
+++ lib/categories.dart
@@ -0,0 +1,13 @@
+class Categories {
+  final String name;
+  Categories({this.name});
+}
+
+List<Categories> dummyCategories = [
+  Categories(name: "Mouse"),
+  Categories(name: "Keyboard"),
+  Categories(name: "PC Gaming"),
+  Categories(name: "Gaming Chair"),
+  Categories(name: "Mouse Pad"),
+  Categories(name: "Headset"),
+];
---
+++ lib/product.dart
@@ -0,0 +1,55 @@
+class Product {
+  final String name;
+  final String brand;
+  final String store;
+  final String price;
+  final String urlimage;
+  Product({this.name, this.brand, this.store, this.price, this.urlimage});
+}
+
+List<Product> dummyProduct = [
+  Product(
+      name: "Laptop Gaming Acer Nitro 5 AN515 52 51T2 ( NH.Q3MSN.002 )",
+      brand: "Acer",
+      store: "itgaleriofficial",
+      price: "10.640.000",
+      urlimage: "images/laptop1.jpg"),
+  Product(
+      name: "Armaggedddon 7.1 Surround Sound Gaming RGB Headset NUKE 11",
+      brand: "Armaggedddon",
+      store: "officialarmaggeddon",
+      price: "344.000",
+      urlimage: "images/headset1.jpg"),
+  Product(
+      name:
+          "Mouse USB berkabel Wired USB Mouse competitive game notebook light USB Mice",
+      brand: "",
+      store: "whimsicalworld.id",
+      price: "34.199",
+      urlimage: "images/mouse1.jpg"),
+  Product(
+      name: "Mousepad Gaming 29X25 cm",
+      brand: "Razer",
+      store: "obambacomp",
+      price: "10.000",
+      urlimage: "images/mousepad1.jpg"),
+  Product(
+      name:
+          "Sandisk Flashdisk OTG 32GB/64GB/128GB/256GB USB Flash Drive 130M/S USB3.0 Original",
+      brand: "Sandisk",
+      store: "3clsls.id",
+      price: "180.000",
+      urlimage: "images/flashdisk1.jpg"),
+  Product(
+      name: "SADES DRACO GAMING CHAIR",
+      brand: "SADES",
+      store: "officialsades",
+      price: "4.931.000",
+      urlimage: "images/gamingchair1.jpg"),
+  Product(
+      name: "Molucca SB-119 USB Mouse & Keyboard Gaming Combo 1 SET",
+      brand: "Molucca",
+      store: "Accessories Computer Shop",
+      price: "55.000",
+      urlimage: "images/keyboard1.png"),
+];
